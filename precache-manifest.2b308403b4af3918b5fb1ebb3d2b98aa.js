cc36313f8c73cd7fae9d2324210071a674965ab4
Ekamjot Singh <ejot60@gmail.com>
2020-08-18 12:12:27 +0530

    Updates

---
+++ precache-manifest.2b308403b4af3918b5fb1ebb3d2b98aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664942ee27aced9eeaa4ce6b90a9e65a",
+    "url": "/advicegiver/index.html"
+  },
+  {
+    "revision": "360052a0fa46d2539086",
+    "url": "/advicegiver/static/css/main.ea2ae0c3.chunk.css"
+  },
+  {
+    "revision": "0818a435cae1f52db53f",
+    "url": "/advicegiver/static/js/2.8466c36c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advicegiver/static/js/2.8466c36c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360052a0fa46d2539086",
+    "url": "/advicegiver/static/js/main.795e462e.chunk.js"
+  },
+  {
+    "revision": "506a1bfd895030b18c24",
+    "url": "/advicegiver/static/js/runtime-main.7838d644.js"
+  },
+  {
+    "revision": "529efd890a220c29eae1d438fe0bc0fb",
+    "url": "/advicegiver/static/media/bg.529efd89.jpg"
+  }
+]);
